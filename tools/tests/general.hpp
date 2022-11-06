37148cfc9791fa3c04e653f82aae50071b4e4760
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-11-06 18:46:05 -0500

    use 64-bit Hex representation for message output

--- tools/tests/general.hpp
+++ tools/tests/general.hpp
@@ -5,5 +5,5 @@
 using namespace Dragon;
 
 void mCallback(Message::Message* pMessage) {
-    printf("Message Recieved:\n\tEngine:\t\t%s\n\tCode:\t\t%I64i\n\tMessage:\t%s", pMessage->engineName.c_str(), pMessage->code, pMessage->message.c_str());
+    printf("Message Recieved:\n\tEngine:\t\t%s\n\tCode:\t\t%llx\n\tMessage:\t%s", pMessage->engineName.c_str(), pMessage->code, pMessage->message.c_str());
 }
