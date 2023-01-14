9e8f63935300684fb4e5908b112b08334af1143e
Khrysys <admin@khrysystem.dev>
2023-01-14 00:32:29 -0500

    Build up unit test, remove debug from action

---
+++ tools/tests/unit_test_eval_functions.cpp
@@ -0,0 +1,9 @@
+#include <dragon/dragon.hpp>
+
+int main(void) {
+	DgEngine* engine;
+	dgCreateEngine(engine);
+	dgCreateWindow(engine, "Win", 300, 300, false);
+	dgUpdate(engine);
+	dgTerminateEngine(engine);
+}
