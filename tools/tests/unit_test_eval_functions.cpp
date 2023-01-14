003ccd493dbcc0207eee6089a28a5a428170be7b
Khrysys <admin@khrysystem.dev>
2023-01-14 15:48:15 -0500

    Add more debugging

--- tools/tests/unit_test_eval_functions.cpp
+++ tools/tests/unit_test_eval_functions.cpp
@@ -6,11 +6,13 @@ int main(void) {
 		std::cerr << "dgCreateEngine failed" << std::endl;
 		return 1;
 	}
+	std::cout << "dgCreateEngine successful" << std::endl;
 		
 	if (!dgCreateWindow(&engine, "Win", 300, 300, false)) {
 		std::cerr << "dgCreateWindow failed" << std::endl;
 		return 2;
 	}
+	std::cout << "dgCreateWindow successful" << std::endl;
 
 	dgUpdate(&engine);
 	dgTerminateEngine(&engine);
