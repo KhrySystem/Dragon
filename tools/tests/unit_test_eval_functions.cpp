5a5caf5c3a375a0bcedbafa4afcd2a2de7b26363
Khrysys <admin@khrysystem.dev>
2023-01-14 15:16:21 -0500

    Add debug output to unit test

--- tools/tests/unit_test_eval_functions.cpp
+++ tools/tests/unit_test_eval_functions.cpp
@@ -2,8 +2,17 @@
 
 int main(void) {
 	DgEngine engine;
-	dgCreateEngine(&engine);
-	dgCreateWindow(&engine, "Win", 300, 300, false);
+	if (!dgCreateEngine(&engine)) {
+		std::cerr << "dgCreateEngine failed" << std::endl;
+		return 1;
+	}
+		
+	if (!dgCreateWindow(&engine, "Win", 300, 300, false)) {
+		std::cerr << "dgCreateWindow failed" << std::endl;
+		return 2;
+	}
+
 	dgUpdate(&engine);
 	dgTerminateEngine(&engine);
+	return 0;
 }
